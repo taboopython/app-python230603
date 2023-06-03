39c09a00948e9c4bf993be2940c34b0bc24d0371
saboten <hellofromeizo@gmail.com>
2023-06-03 16:16:35 +0900

    ver4

--- src/main.py
+++ src/main.py
@@ -1,3 +1,30 @@
-git push -u origin master
-Branch 'master' set up to track remote branch 'master' from 'origin'.
-Everything up-to-date
+import csv
+import os
+import re
+import pytesseract
+from pdf2image import convert_from_path
+
+# ファイルのパスを環境変数から取得
+pdf_path = os.getenv('PDF_PATH', 'example.pdf')
+csv_path = os.getenv('CSV_PATH', 'output.csv')
+
+# PDFを画像に変換
+images = convert_from_path(pdf_path)
+
+# 画像からテキストを抽出
+texts = []
+for i in range(len(images)):
+    pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+    config = '--psm 6 -l jpn'
+    texts.append(pytesseract.image_to_string(images[i], config=config))
+
+# テキストを解析
+lines = []
+for text in texts:
+    lines.extend(text.split('\n'))
+
+# CSVファイルに書き込む
+with open(csv_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+    for line in lines:
+        writer.writerow([line])
